d6f6da11f14e9a9a4ffbdf1c1a649f3dd897bd56
Honglin Chen <serenahonglin@gmail.com>
2018-06-07 20:02:32 +0800

    change variable name...

--- ASTNodes.h
+++ ASTNodes.h
@@ -1,249 +1,249 @@
-#ifndef SLANG_ASTNODES_H
-#define SLANG_ASTNODES_H
+#ifndef ASTNODES_H
+#define ASTNODES_H
 
 #include <iostream>
 #include <vector>
 #include <llvm/IR/Value.h>
 
-class CodeGenContext;
+class GenCodeContext;
 
-class NExpression;
+class AST_Expression;
 
-class NStatement;
+class AST_Statement;
 
-class NVariableDeclaration;
+class AST_VariableDeclaration;
 
-typedef std::vector<std::shared_ptr<NExpression>> ExpressionList;
-typedef std::vector<std::shared_ptr<NStatement>> StatementList;
-typedef std::vector<std::shared_ptr<NVariableDeclaration>> VariableList;
+typedef std::vector<std::shared_ptr<AST_Expression>> AST_ExpressionList;
+typedef std::vector<std::shared_ptr<AST_Statement>> AST_StatementList;
+typedef std::vector<std::shared_ptr<AST_VariableDeclaration>> AST_VariableList;
 
-class Node
+class AST_Node
 {
 public:
-    Node() = default;
+    AST_Node() = default;
 
-    virtual ~Node() = default;
+    virtual ~AST_Node() = default;
 
-    virtual llvm::Value *codeGen(CodeGenContext &context)
+    virtual llvm::Value *generateCode(GenCodeContext &context)
     { return static_cast<llvm::Value *>(nullptr); }
 };
 
-class NExpression : public Node
+class AST_Expression : public AST_Node
 {
 public:
-    NExpression() = default;
+    AST_Expression() = default;
 };
 
-class NStatement : public Node
+class AST_Statement : public AST_Node
 {
 public:
-    NStatement() = default;
+    AST_Statement() = default;
 };
 
-class NDouble : public NExpression
+class AST_Double : public AST_Expression
 {
 public:
     double value;
 
-    NDouble() = default;
+    AST_Double() = default;
 
-    explicit NDouble(double value) : value(value)
+    explicit AST_Double(double value) : value(value)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NInteger : public NExpression
+class AST_Integer : public AST_Expression
 {
 public:
     uint64_t value;
 
-    NInteger() = default;
+    AST_Integer() = default;
 
-    explicit NInteger(uint64_t value) : value(value)
+    explicit AST_Integer(uint64_t value) : value(value)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 
-    operator NDouble() const
+    operator AST_Double() const
     {
-        return NDouble(value);
+        return AST_Double(value);
     }
 };
 
-class NIdentifier : public NExpression
+class AST_Identifier : public AST_Expression
 {
 public:
     std::string name;
     bool isType = false;
 
-    NIdentifier() = default;
+    AST_Identifier() = default;
 
-    explicit NIdentifier(std::string &name) : name(name)
+    explicit AST_Identifier(std::string &name) : name(name)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NMethodCall : public NExpression
+class AST_MethodCall : public AST_Expression
 {
 public:
-    const std::shared_ptr<NIdentifier> id;
-    std::shared_ptr<ExpressionList> arguments = std::make_shared<ExpressionList>();
+    const std::shared_ptr<AST_Identifier> id;
+    std::shared_ptr<AST_ExpressionList> arguments = std::make_shared<AST_ExpressionList>();
 
-    NMethodCall() = default;
+    AST_MethodCall() = default;
 
-    explicit NMethodCall(const std::shared_ptr<NIdentifier> id) : id(id)
+    explicit AST_MethodCall(const std::shared_ptr<AST_Identifier> id) : id(id)
     {}
 
-    NMethodCall(const std::shared_ptr<NIdentifier> id, std::shared_ptr<ExpressionList> arguments) :
+    AST_MethodCall(const std::shared_ptr<AST_Identifier> id, std::shared_ptr<AST_ExpressionList> arguments) :
             id(id),
             arguments(arguments)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NBinaryOperator : public NExpression
+class AST_BinaryOperator : public AST_Expression
 {
 public:
     int op;
-    std::shared_ptr<NExpression> lhs;
-    std::shared_ptr<NExpression> rhs;
+    std::shared_ptr<AST_Expression> lhs;
+    std::shared_ptr<AST_Expression> rhs;
 
-    NBinaryOperator() = default;
+    AST_BinaryOperator() = default;
 
-    NBinaryOperator(std::shared_ptr<NExpression> lhs, int op, std::shared_ptr<NExpression> rhs) :
+    AST_BinaryOperator(std::shared_ptr<AST_Expression> lhs, int op, std::shared_ptr<AST_Expression> rhs) :
             lhs(lhs),
             op(op),
             rhs(rhs)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NAssignment : public NExpression
+class AST_Assignment : public AST_Expression
 {
 public:
-    std::shared_ptr<NIdentifier> lhs;
-    std::shared_ptr<NExpression> rhs;
+    std::shared_ptr<AST_Identifier> lhs;
+    std::shared_ptr<AST_Expression> rhs;
 
-    NAssignment() = default;
+    AST_Assignment() = default;
 
-    NAssignment(std::shared_ptr<NIdentifier> lhs, std::shared_ptr<NExpression> rhs) :
+    AST_Assignment(std::shared_ptr<AST_Identifier> lhs, std::shared_ptr<AST_Expression> rhs) :
             lhs(lhs),
             rhs(rhs)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NBlock : public NExpression
+class AST_Block : public AST_Expression
 {
 public:
-    std::shared_ptr<StatementList> statements = std::make_shared<StatementList>();
+    std::shared_ptr<AST_StatementList> statements = std::make_shared<AST_StatementList>();
 
-    NBlock() = default;
+    AST_Block() = default;
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NExpressionStatement : public NStatement
+class AST_ExpressionStatement : public AST_Statement
 {
 public:
-    std::shared_ptr<NExpression> expression;
+    std::shared_ptr<AST_Expression> expression;
 
-    NExpressionStatement() = default;
+    AST_ExpressionStatement() = default;
 
-    NExpressionStatement(std::shared_ptr<NExpression> expression)
+    AST_ExpressionStatement(std::shared_ptr<AST_Expression> expression)
             : expression(expression)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NVariableDeclaration : public NStatement
+class AST_VariableDeclaration : public AST_Statement
 {
 public:
-    const std::shared_ptr<NIdentifier> type;
-    std::shared_ptr<NIdentifier> id;
-    std::shared_ptr<NExpression> assignmentExpr = nullptr;
+    const std::shared_ptr<AST_Identifier> type;
+    std::shared_ptr<AST_Identifier> id;
+    std::shared_ptr<AST_Expression> assignmentExpr = nullptr;
 
-    NVariableDeclaration() = default;
+    AST_VariableDeclaration() = default;
 
-    NVariableDeclaration(const std::shared_ptr<NIdentifier> type, std::shared_ptr<NIdentifier> id,
-                         std::shared_ptr<NExpression> assignmentExpr = nullptr) :
+    AST_VariableDeclaration(const std::shared_ptr<AST_Identifier> type, std::shared_ptr<AST_Identifier> id,
+                         std::shared_ptr<AST_Expression> assignmentExpr = nullptr) :
             type(type),
             id(id),
             assignmentExpr(assignmentExpr)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NFunctionDeclaration : public NStatement
+class AST_FunctionDeclaration : public AST_Statement
 {
 public:
-    std::shared_ptr<NIdentifier> type;
-    std::shared_ptr<NIdentifier> id;
-    std::shared_ptr<VariableList> arguments = std::make_shared<VariableList>();
-    std::shared_ptr<NBlock> block;
+    std::shared_ptr<AST_Identifier> type;
+    std::shared_ptr<AST_Identifier> id;
+    std::shared_ptr<AST_VariableList> arguments = std::make_shared<AST_VariableList>();
+    std::shared_ptr<AST_Block> block;
 
-    NFunctionDeclaration() = default;
+    AST_FunctionDeclaration() = default;
 
-    NFunctionDeclaration(std::shared_ptr<NIdentifier> type, std::shared_ptr<NIdentifier> id,
-                         std::shared_ptr<VariableList> arguments, std::shared_ptr<NBlock> block) :
+    AST_FunctionDeclaration(std::shared_ptr<AST_Identifier> type, std::shared_ptr<AST_Identifier> id,
+                         std::shared_ptr<AST_VariableList> arguments, std::shared_ptr<AST_Block> block) :
             type(type),
             id(id),
             arguments(arguments),
             block(block)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NReturnStatement : public NStatement
+class AST_ReturnStatement : public AST_Statement
 {
 public:
-    std::shared_ptr<NExpression> expression;
+    std::shared_ptr<AST_Expression> expression;
 
-    NReturnStatement() = default;
+    AST_ReturnStatement() = default;
 
-    explicit NReturnStatement(std::shared_ptr<NExpression> expression) : expression(expression)
+    explicit AST_ReturnStatement(std::shared_ptr<AST_Expression> expression) : expression(expression)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NIfStatement : public NStatement
+class AST_IfStatement : public AST_Statement
 {
 public:
-    std::shared_ptr<NExpression> condition;
-    std::shared_ptr<NBlock> trueBlock;
-    std::shared_ptr<NBlock> falseBlock;
+    std::shared_ptr<AST_Expression> condition;
+    std::shared_ptr<AST_Block> trueBlock;
+    std::shared_ptr<AST_Block> falseBlock;
 
-    NIfStatement() = default;
+    AST_IfStatement() = default;
 
-    NIfStatement(std::shared_ptr<NExpression> condition, std::shared_ptr<NBlock> trueBlock,
-                 std::shared_ptr<NBlock> falseBlock = nullptr) :
+    AST_IfStatement(std::shared_ptr<AST_Expression> condition, std::shared_ptr<AST_Block> trueBlock,
+                 std::shared_ptr<AST_Block> falseBlock = nullptr) :
             condition(condition),
             trueBlock(trueBlock),
             falseBlock(falseBlock)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NForStatement : public NStatement
+class AST_ForStatement : public AST_Statement
 {
 public:
-    std::shared_ptr<NExpression> initial, condition, increment;
-    std::shared_ptr<NBlock> block;
+    std::shared_ptr<AST_Expression> initial, condition, increment;
+    std::shared_ptr<AST_Block> block;
 
-    NForStatement() = default;
+    AST_ForStatement() = default;
 
-    NForStatement(std::shared_ptr<NBlock> block, std::shared_ptr<NExpression> initial = nullptr,
-                  std::shared_ptr<NExpression> condition = nullptr, std::shared_ptr<NExpression> increment = nullptr) :
+    AST_ForStatement(std::shared_ptr<AST_Block> block, std::shared_ptr<AST_Expression> initial = nullptr,
+                  std::shared_ptr<AST_Expression> condition = nullptr, std::shared_ptr<AST_Expression> increment = nullptr) :
             block(block),
             initial(initial),
             condition(condition),
@@ -251,27 +251,27 @@ public:
     {
         if (condition == nullptr)
         {
-            condition = std::make_shared<NInteger>(1);
+            condition = std::make_shared<AST_Integer>(1);
         }
     }
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-class NLiteral : NExpression
+class AST_Literal : AST_Expression
 {
 public:
     std::string value;
 
-    NLiteral() = default;
+    AST_Literal() = default;
 
-    NLiteral(const std::string str) :
+    AST_Literal(const std::string str) :
             value(str)
     {}
 
-    virtual llvm::Value *codeGen(CodeGenContext &context) override;
+    virtual llvm::Value *generateCode(GenCodeContext &context) override;
 };
 
-std::unique_ptr<NExpression> LogError(std::string str);
+std::unique_ptr<AST_Expression> LogError(std::string str);
 
-#endif //SLANG_ASTNODES_H
+#endif //ASTNODES_H
